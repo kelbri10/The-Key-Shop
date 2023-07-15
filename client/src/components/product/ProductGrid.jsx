c02f9782c37391b89841ac0b13c8971911cee1e3
Kelsey Hughes <kelbri10@gmail.com>
2023-07-15 10:38:57 -0400

    updated checkboxes to filter unique values using Set constructor

--- client/src/components/product/ProductGrid.jsx
+++ client/src/components/product/ProductGrid.jsx
@@ -1,22 +1,23 @@
 import { useState, useEffect } from 'react'; 
 import axios from "axios";
 
+
 const ProductGrid = () => { 
 
     const [products, setProducts] = useState([]); 
+    // const [isChecked, setIsChecked] = useState(!isChecked); 
 
     const fetchProducts = async() => { 
         let response = await axios('http://localhost:3080/products'); 
         let products = await response.data; 
         setProducts(products);
-        console.log(products) 
     }
 
     useEffect(() => { 
         fetchProducts(); 
     }, []); 
 
-    const gridItems = products.map(product => (
+    let gridItems = products.map(product => (
         <div key={product._id} data-filter-type={product.type}>
             <img src='https://source.unsplash.com/random/400x400/?keyboard' alt="random unsplash image"/>
             <p>{product.name}</p>
@@ -25,6 +26,18 @@ const ProductGrid = () => {
         </div>
     )); 
 
+    let productTypes = products.map(product => product.type); 
+    let uniqueProductTypes = [...new Set(productTypes)]; 
+    let checkboxes = uniqueProductTypes.map(productType => (
+        <div>
+            <input type="checkbox" 
+                id={productType}
+                name={productType}
+                value={productType} />
+            {productType}
+        </div>
+
+    ))
     // add a sidebar that has the buttons to help filter the products and sets the data-attibute-type to keyboards, switches, etc 
     //if the user clicks one of the buttons it causes a filter array method to act on the products and renders the inside to show the correct type 
     //ex. products.filter(product => return product.type === filterParam); 
@@ -33,7 +46,13 @@ const ProductGrid = () => {
 
     return(
         <>
-            {gridItems}
+            <div className="checkbox-grid">
+                {checkboxes}
+            </div>
+
+            <div className="product-grid">
+                {gridItems}
+            </div>
         </>
     )
 
