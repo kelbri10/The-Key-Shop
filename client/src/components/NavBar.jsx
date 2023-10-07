8302313e545b4f4175d02436e012d9344688701f
Kelsey Hughes <kelbri10@gmail.com>
2023-10-07 17:41:13 -0400

    navbar displays number of total items in cart, not just length of array

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,18 +3,30 @@ import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import { NavLink } from "react-router-dom";
 
 import CartContext from "../CartContext";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 const NavBar = () => { 
     const { items } = useContext(CartContext);  
     const [isOpen, setIsOpen] = useState(false); 
+    const [numOfItems, setNumOfItems] = useState(0); 
 
     const handleNav = () => { 
         setIsOpen(!isOpen); 
     }
 
     //need to update the items based off the quantity not the items length 
+    useEffect(() => { 
+        const calculateNumOfItems = () => { 
+            let temp = [...items]; 
+            let tempQty = temp.map(el => el.qty)
+            let sum = tempQty.reduce((acc, curr) => parseInt(acc) + parseInt(curr), 0)
     
+            setNumOfItems(sum); 
+            return numOfItems; 
+        }
+
+        calculateNumOfItems(); 
+    }, [items])
 
     return(
             <nav className="border-solid border-b border-cyan-900 flex flex-row justify-between items-center px-10 py-4">
@@ -32,7 +44,7 @@ const NavBar = () => {
                         <li className="hover:underline hover:text-cyan-600"> <NavLink to={"/contact"}>Contact</NavLink></li>
                         <li className="flex flex-row gap-2">
                             <NavLink to={"/cart"}><FontAwesomeIcon icon={faCartShopping} /></NavLink>
-                            <p className="bg-slate-100 rounded-full text-center text-stone-900 px-4">{items.length}</p>
+                            <p className="bg-slate-100 rounded-full text-center text-stone-900 px-4">{numOfItems}</p>
                         </li>
                     </ul>: null }
                     
@@ -45,7 +57,7 @@ const NavBar = () => {
 
                 <div className="hidden md:flex md:flex-row md:items-center md:space-x-4">
                     <NavLink to={"/cart"}><FontAwesomeIcon icon={faCartShopping} /></NavLink>
-                    <p>{items.length}</p>
+                    <p>{numOfItems}</p>
                 </div>
             </nav>
     )
