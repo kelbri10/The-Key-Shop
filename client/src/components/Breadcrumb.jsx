9730f2a4165b4cd5616dc8f39aa893959d1377d4
Kelsey Hughes <kelbri10@gmail.com>
2023-10-10 13:15:21 -0400

    fixed broken link in breadcrumb

--- client/src/components/Breadcrumb.jsx
+++ client/src/components/Breadcrumb.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const BreadCrumb = ({product}) => { 
     return (
         <div>
-            <p className="p-10 bg-slate-400 text-slate-100 font-bold tracking-wide"> shop / <Link to="/shop">collection</Link> / {product} </p>
+            <p className="p-10 bg-slate-400 text-slate-100 font-bold tracking-wide"> shop / <Link to="/shop/all">collection</Link> / {product} </p>
         </div>
     ); 
 }
