187968d4d54dcaeff00697c3cf4be3f5a2222a1b
Kelsey Hughes <kelbri10@gmail.com>
2023-10-10 16:02:59 -0400

    removed unused imports

--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -1,17 +1,17 @@
-import Button from '../components/Button'; 
 import AlertBanner from '../components/AlertBanner'; 
 import NavBar  from '../components/NavBar';
-import ProductGrid from './fragments/ProductGrid';
 import Footer from "../components/footer/Footer"; 
 import { Outlet } from 'react-router-dom';
 const Shop = () => { 
     return( 
-        <div>
+        <div className="flex flex-col justify-between">
             <AlertBanner alertText={{message: 'Free shipping for orders over $100'}} />
            
             <NavBar />
+
             
             <Outlet/>
+            
             
             <Footer />
         </div>
